84fb9e709999d61aca169f754a946e43697a49cf
AdahMilly <oamildred@gmail.com>
2024-01-02 17:01:05 +0300

    lists

--- Basics/lists.py
+++ Basics/lists.py
@@ -10,3 +10,53 @@ Dictionary is a collection which is ordered** and changeable. No duplicate membe
 '''
 fruits = ["apple", "banana", "cherry"]
 print(fruits)
+
+#list items are ordered, changeable, and allow duplicate values
+#items in a list - len() function
+cars = ["audi", "bmw", "toyota", "nissan"]
+print(len(cars))
+
+#you can use the list constructor when creating new lists
+mylist = list(("apple", "banana", "cherry"))
+
+#accessing items in a list
+mfruits = ["apple", "banana", "cherry"]
+print(mfruits[0])
+
+#you can specify range of indexes
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:5])
+
+#change items in a list
+ff = ["apple", "banana", "cherry"]
+ff[1] = "blackcurrant"
+print(ff)
+
+#insert items - use the insert method
+thislist = ["apple", "banana", "cherry"]
+thislist.insert(2, "watermelon")
+print(thislist)
+
+#add an item at the end of list - use the append() method
+thislist = ["apple", "banana", "cherry"]
+thislist.append("orange")
+print(thislist)
+
+#add elements from another list to the current list - use extend() method
+thislist = ["apple", "banana", "cherry"]
+tropical = ["mango", "pineapple", "papaya"]
+thislist.extend(tropical)
+print(thislist)
+
+#remove specified item - use remove() method
+thislist = ["apple", "banana", "cherry"]
+thislist.remove("banana")
+print(thislist)
+
+#remove specified index - use the pop() method
+thislist = ["apple", "banana", "cherry"]
+thislist.pop(1)
+print(thislist)
+
+#del keyword removes specified index, or delete entire list
+#clear - empties the list
